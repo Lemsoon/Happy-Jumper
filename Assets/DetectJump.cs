3799d2ea92e175cff11950fe8325118f8e4460e5
Lemsoon <selfishworm117@gmail.com>
2023-01-15 15:36:10 +0100

    i dont know

--- Assets/DetectJump.cs
+++ Assets/DetectJump.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class DetectJump : MonoBehaviour
 {
+    private Animator _animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collider2D col)
     {
-        
+        _animator.SetBool("JumpedOn", true);
+
+    }
+
+    void NotJumped(Collider2D col)
+    {
+        _animator.SetBool("JumpedOn", false);
     }
 }
