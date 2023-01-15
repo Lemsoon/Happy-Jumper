c6874bc6e9e7e38f594ddda13b21769169587ed7
Lemsoon <selfishworm117@gmail.com>
2023-01-15 15:18:21 +0100

    Added new jump sound
    Will probably go back to old jump sound and delete new one. New sound is bad PLING

--- Assets/DetectJump.cs
+++ Assets/DetectJump.cs
@@ -2,20 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartGame : MonoBehaviour
+public class DetectJump : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Time.timeScale = 0;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount > 0 || Input.GetKey("w"))
-        {
-            Time.timeScale = 1;
-        }
+        
     }
 }
